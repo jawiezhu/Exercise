4dec390ba57c112834931c631fdb596857bcc8ba
jawiezhu <JawieZhu@jawie.local>
2015-03-05 10:08:18 +0800

    150305

---
+++ Tree/non-recursion.cpp
@@ -0,0 +1,16 @@
+
+class Solution{
+public:
+	vector<int> preorderTraversal(TreeNode * root){
+
+	}
+
+	vector<int> inorderTraversal(TreeNode * root){
+
+	}
+
+	vector<int> postorderTraversal(TreeNode * root){
+
+	}
+	
+}
